b2e210e5b6aa49c143f26ce2ce1591d66b3511cb
Seono <pianist5555@gmail.com>
2020-03-18 04:28:08 +0900

    0318

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Counter from './components/Counter.js'
 import Todos from './components/Todos.js'
-//import ColorBox from './components/ColorBox.js';
-// Provider를 사용하기위해 context를 임포트 사용했다면 기본값을 넣어주어야 오류가 나지 않음
-// import ColorContext from './contexts/color.js';
-//import { ColorProvider } from './contexts/color.js'
-//import SelectColors from './components/SelectColors.js';
+/*
+import ColorBox from './components/ColorBox.js';
+Provider를 사용하기위해 context를 임포트 사용했다면 기본값을 넣어주어야 오류가 나지 않음
+import ColorContext from './contexts/color.js';
+import { ColorProvider } from './contexts/color.js'
+import SelectColors from './components/SelectColors.js';
+*/
 
 function App() {
   return (
@@ -14,14 +16,16 @@ function App() {
       <hr />
       <Todos />
     </div>
-    /*레거시 : <ColorContext.Provider value={{color:'green'}}>
+    /*
+    레거시 : <ColorContext.Provider value={{color:'green'}}>
     <ColorProvider>
       <div>
         <SelectColors />
         <ColorBox></ColorBox>
       </div>
     </ColorProvider>
-    레거시 : </ColorContext.Provider>*/
+    레거시 : </ColorContext.Provider>
+    */
   );
 }
 
