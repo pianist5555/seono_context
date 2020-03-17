56a37bafe04f814e2d87030776509a5c62c7a4c6
Seono <pianist5555@gmail.com>
2020-03-18 03:44:50 +0900

    0318 redux init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import ColorBox from './components/ColorBox.js';
+import Counter from './components/Counter.js'
+import Todos from './components/Todos.js'
+//import ColorBox from './components/ColorBox.js';
 // Provider를 사용하기위해 context를 임포트 사용했다면 기본값을 넣어주어야 오류가 나지 않음
 // import ColorContext from './contexts/color.js';
-
-import { ColorProvider } from './contexts/color.js'
-import SelectColors from './components/SelectColors.js';
+//import { ColorProvider } from './contexts/color.js'
+//import SelectColors from './components/SelectColors.js';
 
 function App() {
   return (
-    //레거시 : <ColorContext.Provider value={{color:'green'}}>
+    <div>
+      <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+    /*레거시 : <ColorContext.Provider value={{color:'green'}}>
     <ColorProvider>
       <div>
         <SelectColors />
         <ColorBox></ColorBox>
       </div>
     </ColorProvider>
-    //레거시 : </ColorContext.Provider>
+    레거시 : </ColorContext.Provider>*/
   );
 }
 
