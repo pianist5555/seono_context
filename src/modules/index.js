4bef94c43c7e82615273b92845deb7a9f902527b
Seono <pianist5555@gmail.com>
2020-03-28 07:50:07 +0900

    0328

---
+++ src/modules/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import counter from './counter.js';
+import todos from './todos';
+
+// 리듀서 함수를 하나로 묶어서 store를 생성해주어야 함
+const rootReducer = combineReducers({
+    counter,
+    todos,
+})
+
+// index.js로 설정해주면 디렉토리 이름만 사용해여 import 할 수 있다. ex) import rootReducer from './modules.js';
+export default rootReducer;
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -28,3 +28,49 @@ export const remove = id => ({
     type: REMOVE,
     id
 });
+
+const initialState = {
+    input : '',
+    todos: [
+        {
+            id: 1,
+            text: '리덕스 기초 배우기',
+            done: true
+        },
+        {
+            id: 2,
+            text: '리액트와 리덕스 사용하기',
+            done : false
+        }
+    ]
+};
+
+function todos(state = initialState, action){
+    switch (action.type) {
+        case CHANGE_INPUT:
+            return {
+                ...state, // 객체 spread 연산자 사용하여 기존 객체의 값을 수정해서 새로운 객체로 인식하게 함(ES9)
+                input : action.input
+            };
+        case INSERT:
+            return {
+                ...state,
+                todos : state.todos.concat(action.todo)
+            };
+        case TOGGLE:
+            return {
+                ...state,
+                todos : state.todos.map(todo =>
+                todo.id === action.id ? {...todo, done: !todo.done}:todo)
+            };
+        case REMOVE:
+            return {
+                ...state,
+                todos : state.todos.filter(todo =>todo.id !== action.id)
+            };
+        default:
+            return state;
+    }
+}
+
+export default todos;
