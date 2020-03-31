3e7a752a015cb78d513a735c9416e971c163b24f
Seono <pianist5555@gmail.com>
2020-03-31 13:34:44 +0900

    0331 static redux end

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,14 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입을 정의한다.
 const INCREASE = '/counter/INCREASE';
 const DECREASE = '/counter/DECREASE'; /* 모듈 이름/액션 이름 */
 
 // 액션 생성 함수를 만든다.
-export const increase = () => ({ type:INCREASE });
-export const decrease = () => ({ type:DECREASE });
+/*export const increase = () => ({ type:INCREASE }); --------------------redux-actions 모듈 사용 전 기존코드 
+export const decrease = () => ({ type:DECREASE });*/
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기값을 설정한다.
 const initialState = {
@@ -12,7 +16,7 @@ const initialState = {
 };
 
 // 리듀서를 만든다.
-function counter (state = initialState, action){
+/*function counter (state = initialState, action){ ---------------------redux-actions 모듈 사용 전 기존코드                               
     switch (action.type) {
         case INCREASE:
             console.log("8.리듀서 counter함수")
@@ -26,6 +30,13 @@ function counter (state = initialState, action){
         default:
             return state;
     }
-}
+}*/
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }), // 액션 타입에 모듈 이름/액션 이름으로 되어있기 때문에 []로 감싸줘야함
+        [DECREASE]: (state, action) => ({ number: state.number - 1 })
+    },
+    initialState
+);
 
 export default counter;
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,33 +1,43 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
-export const changeInput = input => ({
-    type: CHANGE_INPUT,
-    input,
-});
-
 let id= 3;
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+export const insert = createAction(INSERT, text => ({
+    id: id++,
+    text,
+    done: false,
+}));
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+// export const changeInput = input => ({
+//     type: CHANGE_INPUT,
+//     input,
+// });
 
-export const insert = text => ({
-    type: INSERT,
-    todo: {
-        id: id++,
-        text,
-        done: false
-    }
-});
+// export const insert = text => ({
+//     type: INSERT,
+//     todo: {
+//         id: id++,
+//         text,
+//         done: false
+//     }
+// });
 
-export const toggle = id => ({
-    type: TOGGLE,
-    id
-});
+// export const toggle = id => ({
+//     type: TOGGLE,
+//     id
+// });
 
-export const remove = id => ({
-    type: REMOVE,
-    id
-});
+
+// export const remove = id => ({
+//     type: REMOVE,
+//     id
+// });
 
 const initialState = {
     input : '',
@@ -45,32 +55,41 @@ const initialState = {
     ]
 };
 
-function todos(state = initialState, action){
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return {
-                state, // 객체 spread 연산자 사용하여 기존 객체의 값을 수정해서 새로운 객체로 인식하게 함(ES9)
-                input : action.input
-            };
-        case INSERT:
-            return {
-                ...state, // https://sheldhe93.tistory.com/13 스프레드 연산자 설명 => 배열 상태를 분해함
-                todos : state.todos.concat(action.todo)
-            };
-        case TOGGLE:
-            return {
-                ...state,
-                todos : state.todos.map(todo =>
-                todo.id === action.id ? {...todo, done: !todo.done}:todo)
-            };
-        case REMOVE:
-            return {
-                ...state,
-                todos : state.todos.filter(todo =>todo.id !== action.id)
-            };
-        default:
-            return state;
-    }
-}
+const todos = handleActions( // payload : input을 받든 todo,id로 받든 actions 모듈에서는 payload로 리듀서는 인식하게 된다.
+    {
+      [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+      [INSERT]: (state, { payload: todo }) => ({ ...state, todos: state.todos.concat(todo) }),
+      [TOGGLE]: (state, { payload: id }) => ({ ...state, todos: state.todos.map(todo => todo.id === id ? {...todo, done: !todo.done}:todo,) }),
+      [REMOVE]: (state, { payload: id }) => ({ ...state, todos: state.todos.filter(todo => todo.id !== id), }),
+    },
+    initialState,
+  );
+// function todos(state = initialState, action){ ------------------------------------------------------------------------ redux-actions 모듈 사용 전 기존코드 
+//     switch (action.type) {
+//         case CHANGE_INPUT:
+//             return {
+//                 ...state, // 객체 spread 연산자 사용하여 기존 객체의 값을 수정해서 새로운 객체로 인식하게 함(ES9)
+//                 input : action.input
+//             };
+//         case INSERT:
+//             return {
+//                 ...state, // https://sheldhe93.tistory.com/13 스프레드 연산자 설명 => 2. object 객체 사용시 스프레드 연산자 
+//                 todos : state.todos.concat(action.todo)
+//             };
+//         case TOGGLE:
+//             return {
+//                 ...state,
+//                 todos : state.todos.map(todo =>
+//                 todo.id === action.id ? {...todo, done: !todo.done}:todo)
+//             };
+//         case REMOVE:
+//             return {
+//                 ...state,
+//                 todos : state.todos.filter(todo =>todo.id !== action.id)
+//             };
+//         default:
+//             return state;
+//     }
+// }
 
 export default todos;
